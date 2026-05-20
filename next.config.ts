b02ea9f005ea81f3d170af8642126585e70deb31
max-ghz <userbarabiasz95@gmail.com>
2026-05-20 12:20:50 +0200

    Fix image on GitHub Pages: let configure-pages inject basePath

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: '',
-    assetPrefix: '',
     images: { unoptimized: true }
 };
 
